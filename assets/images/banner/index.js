8e46af2e48cb42169e37614fd38f9341a837f8a6
Arpit Patidar <arpit.ptdr@hotmail.com>
2023-11-09 17:51:33 +0530

    added hero images

--- assets/images/banner/index.js
+++ assets/images/banner/index.js
@@ -1,8 +1,8 @@
 const sharp = require('sharp');
 const fs = require('fs');
 
-const inputBuffer = fs.readFileSync('./living-room.jpg')
+const inputBuffer = fs.readFileSync('./home-2618511_1280.jpg')
 
 sharp(inputBuffer)
-  .resize(1280, 840)
-  .toFile('./living-room.webp', (err, info) => {});
+  .resize(1280, 720)
+  .toFile('./home.webp', (err, info) => {});
