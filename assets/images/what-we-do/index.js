e491d4c695752cf02916507ad771b8b68a3a63db
Arpit Patidar <arpit.ptdr@hotmail.com>
2023-11-09 19:05:20 +0530

    updated our vision

--- assets/images/what-we-do/index.js
+++
@@ -1,8 +0,0 @@
-const sharp = require('sharp');
-const fs = require('fs');
-
-const inputBuffer = fs.readFileSync('./startup-3267505_640.jpg')
-
-sharp(inputBuffer)
-  .resize(347, 446)
-  .toFile('./design.webp', (err, info) => {});
